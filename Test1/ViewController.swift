b5cc5eae1bb8e88beafd5197be58149468ae18b6
DENHAUT Axel <adenhaut@pdt0537.devinci.fr>
2017-03-21 08:54:46 +0100

    1st commit

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var mTitleLabel : UILabel?
 
+    @IBOutlet var mSegmentedBar : UISegmentedControl?
+    
+    @IBAction func ChangeColor(){
+        NSLog("test")
+        if(mSegmentedBar?.selectedSegmentIndex == 0){
+            mTitleLabel?.textColor = UIColor.init(red: 0, green: 0, blue: 1.0, alpha: 1)
+        }else if(mSegmentedBar?.selectedSegmentIndex == 1){
+            mTitleLabel?.textColor = UIColor.init(red: 1.0, green: 0, blue: 0, alpha: 1)
+        }else{
+            mTitleLabel?.textColor = UIColor.init(red: 0, green: 1.0, blue: 0, alpha: 1)
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mTitleLabel?.text = "Axel"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func IamNotRich() {
+        mTitleLabel?.text = "0€"
+    }
 
-
+    
 }
 
